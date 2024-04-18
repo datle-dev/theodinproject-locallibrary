f76052550bfa971909ac44f8b575067da6682319
datle-dev <dle.dev@outlook.com>
2024-04-18 08:34:30 -0400

    Add mongoose connection to app.js

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log(`mongoose connection readyState = ${mongoose.connection.readyState}`);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');

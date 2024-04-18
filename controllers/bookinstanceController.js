0c1dac73bd10cbe876071576fa25c21f07489519
datle-dev <dle.dev@outlook.com>
2024-04-18 11:59:10 -0400

    Add content and styling to book instance listing

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -3,7 +3,12 @@ const asyncHandler = require('express-async-handler');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance list');
+  const allBookInstances = await BookInstance.find().populate('book').exec();
+
+  res.render('pages/bookinstanceList', {
+    title: 'Book Instance List',
+    bookinstanceList: allBookInstances,
+  });
 });
 
 // Display detail page for a specific BookInstance.
